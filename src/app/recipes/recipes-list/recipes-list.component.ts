ff78dd56f38cf4e730effbebfe4cbf0d9ee4729c
Ustadho_1218 <ustadho@mitrais.com>
2020-02-22 10:19:05 +0700

    create Recipe model and adding content to the Recipes components

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Recipe 1', 'Recipe 1 testing', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake-920x605.jpg'),
+    new Recipe('Recipe 2', 'Recipe 2 testing', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2020/02/Mummy-Meagz-Squeaky-Bean-creme-egg-pancake-stack-920x583.png')
+  ];
   constructor() { }
 
   ngOnInit(): void {
