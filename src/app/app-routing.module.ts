b82d73e0dfc0a1c585160be84783e71725d21d34
Ustadho_1218 <ustadho@mitrais.com>
2020-02-24 19:03:52 +0700

    Adding Editing Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -9,7 +10,9 @@ const appRoutes: Routes = [
     { path: '', redirectTo: 'recipes', pathMatch: 'full'},
     { path: 'recipes', component: RecipesComponent, children: [
       { path: '', component: RecipeStartComponent},
+      { path: 'new', component: RecipeEditComponent}, // harus diatas kalau tidak maka tidak didetek
       { path: ':id', component: RecipesDetailComponent},
+      { path: ':id/edit', component: RecipeEditComponent},
     ]},
     { path: 'shopping-list', component: ShoppingListComponent}
 ];
