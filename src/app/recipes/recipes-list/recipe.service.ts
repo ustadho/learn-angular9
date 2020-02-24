86d6ed3b171de6a55532368cece6d868e7cbce3b
Ustadho_1218 <ustadho@mitrais.com>
2020-02-24 10:33:11 +0700

    Adding Ingredients to Recipes

--- src/app/recipes/recipes-list/recipe.service.ts
+++ src/app/recipes/recipes-list/recipe.service.ts
@@ -1,12 +1,27 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 export class RecipeService {
   receipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Recipe 1', 'Recipe 1 testing', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake-920x605.jpg'),
-    new Recipe('Recipe 2', 'Recipe 2 testing', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2020/02/Mummy-Meagz-Squeaky-Bean-creme-egg-pancake-stack-920x583.png')
+    new Recipe(
+      'Recipe 1',
+      'Recipe 1 testing',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20),
+      ]),
+    new Recipe(
+      'Recipe 2',
+      'Recipe 2 testing',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+      ])
   ];
 
   getRecipes() {
