0d2f97a4b66104df4f9ee28519326ab7941d0406
Ustadho_1218 <ustadho@mitrais.com>
2020-02-25 15:58:26 +0700

    [Form] Adding new Ingredient Controls

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -65,4 +65,12 @@ export class RecipeEditComponent implements OnInit {
 
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        name: new FormControl(),
+        amount: new FormControl()
+      })
+    );
+  }
 }
