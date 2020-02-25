f1f66bacfac1c67bf577b4aba2ca6ce56a66b0de
Ustadho_1218 <ustadho@mitrais.com>
2020-02-25 13:43:39 +0700

    [Form] Allowing the User to Clar (Cancel) the Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -48,4 +48,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy{
     this.editMode = false;
     form.reset();
   }
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
 }
