373b9d2684b401edf1eaebaa9e84ad68ee384fee
Ustadho_1218 <ustadho@mitrais.com>
2020-02-25 13:52:09 +0700

    [Form] Allowing the Deletion of Shopping List Items

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -37,4 +37,9 @@ export class ShoppingListService {
     this.ingredients[index] = newIngredient;
     this.ingredientChanged.next(this.ingredients.slice());
   }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientChanged.next(this.ingredients.slice());
+  }
 }
