634d34247423813750bafbd8d7e2ff9dd1108cd0
Ustadho_1218 <ustadho@mitrais.com>
2020-02-26 13:41:27 +0700

    [Http] Storing Recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { DataStorageService } from './../shared/data-storage.service';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
@@ -7,12 +8,16 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Output() featureSelected = new EventEmitter<string>();
-  constructor() { }
+  constructor(private dataStorageService: DataStorageService) { }
 
   ngOnInit(): void {
   }
 
   onSelect(s: string) {
     this.featureSelected.emit(s);
+  }
+
+  onSaveData() {
+    this.dataStorageService.storeRecipe();
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,19 @@
+import { RecipeService } from './../recipes/recipes-list/recipe.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Recipe } from './../recipes/recipe.model';
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
 
+  }
+
+  storeRecipe() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put('https://ustadho-course-recipe-book.firebaseio.com/recipes.json', recipes)
+    .subscribe(response => {
+      console.log(response);
+    });
   }
 }
