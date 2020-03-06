8d956d416419474078778ea917518305265786a3
Ustadho_1218 <ustadho@mitrais.com>
2020-03-06 17:31:29 +0700

    Updating & Deleting Ingredients

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -23,6 +23,26 @@ export function ShoppingListReducer(
         ...state,
         ingredients: [...state.ingredients, ...action.payload]
       };
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[action.payload.index];
+      const updatedIngredient = {
+        ...ingredient,
+        ...action.payload.ingredient
+      };
+      const updatedIngredients = [...state.ingredients];
+      updatedIngredients[action.payload.index] = updatedIngredient;
+
+      return {
+        ...state,
+        ingredients: updatedIngredients
+      };
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredient: state.ingredients.filter((ig, igIndex) => {
+          return igIndex !== action.payload;
+        })
+      };
     default:
       return state;
   }
