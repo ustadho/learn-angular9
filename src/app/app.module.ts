1359b12f03fc45eb3e6a83d852293e7088c6641c
Ustadho_1218 <ustadho@mitrais.com>
2020-03-10 09:09:03 +0700

    Attaching the Token with an Interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { RecipeService } from './recipes/recipes-list/recipe.service';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -45,7 +46,14 @@ import { AuthComponent } from './auth/auth.component';
     AppRoutingModule,
     StoreModule.forRoot({shoppingList: ShoppingListReducer})
   ],
-  providers: [RecipeService],
+  providers: [
+    RecipeService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -5,40 +5,37 @@ import { Injectable } from '@angular/core';
 import { map, tap, take, exhaustMap } from 'rxjs/operators';
 
 import { Recipe } from './../recipes/recipe.model';
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
   constructor(
     private http: HttpClient,
     private recipeService: RecipeService,
-    private authService: AuthService) {}
+    private authService: AuthService) { }
 
   storeRecipe() {
     const recipes = this.recipeService.getRecipes();
     this.http.put('https://ustadho-course-recipe-book.firebaseio.com/recipes.json', recipes)
-    .subscribe(response => {
-      console.log(response);
-    });
+      .subscribe(response => {
+        console.log(response);
+      });
   }
 
   fetchRecipes() {
-    return this.authService.user.pipe(take(1), exhaustMap(user => {
-      return this.http.get<Recipe[]>(
-        'https://ustadho-course-recipe-book.firebaseio.com/recipes.json',
-        {
-          params: new HttpParams().set('auth', user.token)
-        });
-    }),
-    map(recipes => {
-      return recipes.map(recipe => {
-        return {
-          ...recipe,
-          ingredients: recipe.ingredients ? recipe.ingredients : []
-        };
-      });
-    }),
-    tap(recipes => {
-      this.recipeService.setRecipes(recipes);
-    })
-    );
-    }
+    return this.http
+      .get<Recipe[]>(
+        'https://ustadho-course-recipe-book.firebaseio.com/recipes.json'
+      ).pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
+  }
 }
