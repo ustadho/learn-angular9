0bbe428b0bc2653dbce205bf3e32ddb77639534b
Ustadho_1218 <ustadho@mitrais.com>
2020-03-11 12:44:33 +0700

    [NgRx] And important Note on Actions

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,6 +1,6 @@
 import { Action } from '@ngrx/store';
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
+export const LOGIN = '[Auth] Login';
+export const LOGOUT = '[Auth] Logout';
 
 export class Login implements Action {
   readonly type = LOGIN;
