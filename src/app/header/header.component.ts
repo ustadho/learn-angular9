ed70c1aac37d71db5d51c077089bb58df5ec161f
Ustadho_1218 <ustadho@mitrais.com>
2020-03-12 14:10:14 +0700

    [NgRx] Storing Recipes via Effects

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,6 @@
 import { map } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
-import { AuthService } from './../auth/auth.service';
-import { DataStorageService } from './../shared/data-storage.service';
 import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from '../auth/store/auth.actions';
@@ -19,8 +17,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
   isAuthenticated = false;
 
   constructor(
-    private dataStorageService: DataStorageService,
-    private authService: AuthService,
     private store: Store<fromApp.AppState>) { }
 
   ngOnInit(): void {
@@ -43,7 +39,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   onSaveData() {
-    this.dataStorageService.storeRecipe();
+    // this.dataStorageService.storeRecipe();
+    this.store.dispatch(new RecipesActions.StoreRecipes());
   }
 
   onFetchData() {
--- src/app/recipes/store/recipe.effect.ts
+++ src/app/recipes/store/recipe.effect.ts
@@ -1,9 +1,11 @@
+import { Store } from '@ngrx/store';
 import { HttpClient } from '@angular/common/http';
-import { switchMap, map } from 'rxjs/operators';
+import { switchMap, map, withLatestFrom } from 'rxjs/operators';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import * as RecipesActions from './recipe.actions';
 import { Recipe } from './../recipe.model';
 import { Injectable } from '@angular/core';
+import * as fromApp from '../../store/app.reducer';
 
 @Injectable()
 export class RecipeEffects {
@@ -27,7 +29,18 @@ export class RecipeEffects {
     map(recipes => {
       return new RecipesActions.SetRecipes(recipes);
     })
-    );
+  );
 
-  constructor(private actions$: Actions, private http: HttpClient) {}
+  @Effect({dispatch: false})
+  storeRecipes = this.actions$.pipe(
+    ofType(RecipesActions.STORE_RECIPES),
+    withLatestFrom(this.store.select('recipes')),
+    switchMap(([actionData, recipesState]) => {
+      return this.http.put(
+        'https://ustadho-course-recipe-book.firebaseio.com/recipes.json',
+        recipesState.recipes
+      );
+    }));
+
+  constructor(private actions$: Actions, private http: HttpClient, private store: Store<fromApp.AppState>) { }
 }
